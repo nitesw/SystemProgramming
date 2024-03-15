ddb63f88e681110780ff14d1f62bd924c1a95416
nitesw <apedos2007@gmail.com>
2024-03-15 19:44:36 +0200

    Make a proper code to send an email

--- Lesson_07_EmailSending/Program.cs
+++ Lesson_07_EmailSending/Program.cs
@@ -1,27 +1,40 @@
-﻿using System.Net.Mail;
+﻿using MailKit.Net.Imap;
+using MimeKit;
+using System.IO;
+using System.Net.Mail;
+using MailKit.Net.Smtp;
+using MailKit;
 
 namespace Lesson_07_EmailSending
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            string host = "imap.ukr.net";
+            int port = 993;
             try
             {
-                MailMessage mail = new MailMessage();
-                mail.From = new MailAddress("sendingmails@ukr.net");
-                mail.To.Add("sendingmails@ukr.net");
-                mail.Subject = "Test Mail - 1";
-                mail.Body = "Test";
-                //mail.Attachments.Add(new Attachment(path));
+                MimeMessage mail = new MimeMessage();
+                mail.From.Add(new MailboxAddress("SearchForWord", "sendingmails@ukr.net"));
+                mail.To.Add(new MailboxAddress("Receiver", "sendingmails@ukr.net"));
+                mail.Subject = "Results";
 
-                using (SmtpClient smtpClient = new SmtpClient("smtp.ukr.net"))
+                var bodyBuilder = new BodyBuilder();
+                bodyBuilder.TextBody = "Some info which I found: ";
+                mail.Body = bodyBuilder.ToMessageBody();
+
+                using (var client = new ImapClient())
                 {
-                    smtpClient.Port = 587;
-                    smtpClient.Credentials = new System.Net.NetworkCredential("sendingmails@ukr.net", "MIFchiPSaOnitay3");
-                    smtpClient.EnableSsl = false;
+                    await client.ConnectAsync(host, port, true);
 
-                    smtpClient.Send(mail);
+                    await client.AuthenticateAsync("sendingmails@ukr.net", "r3LkTddNciFBAwoq");
+
+                    var folder = await client.GetFolderAsync("INBOX");
+
+                    await folder.AppendAsync(mail);
+
+                    await client.DisconnectAsync(true);
                 }
             }
             catch (Exception ex)
